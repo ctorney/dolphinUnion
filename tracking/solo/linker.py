a1c308df703f6730bbc9c569eae6043d85cc2a32
Colin Torney <colin.j.torney@gmail.com>
2015-11-26 15:14:36 +0000

    working with real coordinates

--- tracking/solo/linker.py
+++ tracking/solo/linker.py
@@ -17,14 +17,14 @@ FILELIST = HD + '/workspace/dolphinUnion/tracking/solo/fileList.csv'
 df = pd.read_csv(FILELIST)
 
 for index, row in df.iterrows():
-    if index!=2:
+    if index!=0:
         continue
 
     noext, ext = os.path.splitext(row.filename)   
     
     # ********* read in log file and extract position data ********* 
 
-    posName = TRACKDIR + '/POSITIONS_' + str(index) + '_' + noext + '.csv'
+    posName = TRACKDIR + '/CARIBOU_REAL_POS_' + str(index) + '_' + noext + '.csv'
     trackName = TRACKDIR + '/TRACKS_' + str(index) + '_' + noext + '.csv'
     
     toLink = pd.read_csv(posName,index_col=0)
@@ -36,7 +36,8 @@ for index, row in df.iterrows():
     f_iter = (frame for fnum, frame in toLink.groupby('frame'))
     t = pd.concat(pred.link_df_iter(f_iter, 5.0, memory=20))
 
-
+    for cnum, cpos in t.groupby('particle'):
+        print(cnum)
  #   t1 = tp.filtering.filter_stubs(t,2)
 
-    t.to_csv(trackName)
+    t.to_csv(trackName, index=False)
