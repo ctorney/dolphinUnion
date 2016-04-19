a5e6f50b4b3f1b8d4a6aeda3f304163edcca5688
Colin Torney <colin.j.torney@gmail.com>
2016-04-19 18:38:12 +0100

    added moves for multiple time steps

--- analysis/movement/preprocessData.py
+++ analysis/movement/preprocessData.py
@@ -24,28 +24,30 @@ OUTDIR =  HD + '/Dropbox/dolphin_union/2015_footage/Solo/processedTracks/'
 
 df = pd.read_csv(FILELIST)
 allDF = pd.DataFrame()
-for index, row in df.iterrows():
-    noext, ext = os.path.splitext(row.filename)   
-    posfilename = OUTDIR + '/TRACKS_' + str(index) + '_' + noext + '.csv'
-    gridfilename = DATADIR + '/GRID_' + str(index) + '_' + noext + '.npy'
-    gridPosfilename = DATADIR + '/GRIDPOS_' + str(index) + '_' + noext + '.npy'
+for indexF, rowF in df.iterrows():
+    noext, ext = os.path.splitext(rowF.filename)   
+    posfilename = OUTDIR + '/TRACKS_' + str(indexF) + '_' + noext + '.csv'
+    gridfilename = DATADIR + '/GRID_' + str(indexF) + '_' + noext + '.npy'
+    gridPosfilename = DATADIR + '/GRIDPOS_' + str(indexF) + '_' + noext + '.npy'
     posDF = pd.read_csv(posfilename) 
-    posDF['clip']=index
+    posDF['clip']=indexF
     posDF = posDF[posDF['frame']%60==0]
     dt = 60 # 60 frames is 1 second
-    posDF['dtheta']=np.NaN
+    posDF['move1']=np.NaN
+    posDF['move2']=np.NaN
+    posDF['move5']=np.NaN
+    posDF['move10']=np.NaN
     posDF['env_heading']=np.NaN
     for index, row in posDF.iterrows():
         thisFrame =  row['frame']
         thisID = row['c_id']
-       
+        thisX = row['x']
+        thisY = row['y']
+            
         thisTheta = row['heading']
         # calculate the change in heading from this point to the next
         nextTime = posDF[(np.abs(posDF['frame']-(thisFrame+dt))<1e-6)&(posDF['c_id']==thisID)]
         if len(nextTime)==1:
-            posDF.ix[index,'dtheta'] = nextTime.iloc[0]['heading'] -  thisTheta
-            thisX = nextTime.iloc[0]['x']
-            thisY = nextTime.iloc[0]['y']
             # calculate the average heading all the other caribou were taking at this point
             excThis = posDF[posDF.c_id!=thisID]
             xp = excThis['x'].values
@@ -59,12 +61,37 @@ for index, row in df.iterrows():
             yav = np.sum(weights*ydirs)/np.sum(weights)
             posDF.ix[index,'env_heading']  = math.atan2(yav,xav)-  thisTheta
             
+            # 1 second move heading
+            dx = nextTime.iloc[0]['x'] - thisX
+            dy = nextTime.iloc[0]['y'] - thisY
+            posDF.ix[index,'move1'] = math.atan2(dy,dx) -  thisTheta
+        nextTime = posDF[(np.abs(posDF['frame']-(thisFrame+2*dt))<1e-6)&(posDF['c_id']==thisID)]
+        if len(nextTime)==1:
+            # 2 second move heading
+            dx = nextTime.iloc[0]['x'] - thisX
+            dy = nextTime.iloc[0]['y'] - thisY
+            posDF.ix[index,'move2'] = math.atan2(dy,dx) -  thisTheta
+        nextTime = posDF[(np.abs(posDF['frame']-(thisFrame+5*dt))<1e-6)&(posDF['c_id']==thisID)]
+        if len(nextTime)==1:
+            # 5 second move heading
+            dx = nextTime.iloc[0]['x'] - thisX
+            dy = nextTime.iloc[0]['y'] - thisY
+            posDF.ix[index,'move5'] = math.atan2(dy,dx) -  thisTheta
+        
+        nextTime = posDF[(np.abs(posDF['frame']-(thisFrame+10*dt))<1e-6)&(posDF['c_id']==thisID)]
+        if len(nextTime)==1:
+            # 10 second move heading
+            dx = nextTime.iloc[0]['x'] - thisX
+            dy = nextTime.iloc[0]['y'] - thisY
+            posDF.ix[index,'move10'] = math.atan2(dy,dx) -  thisTheta
+        
+            
 
 
     allDF = allDF.append(posDF,ignore_index=True)
 
     
-allDF = allDF[np.isfinite(allDF['dtheta'])]
+allDF = allDF[np.isfinite(allDF['move1'])]
 allDF = allDF.reset_index(drop=True)
 dsize = len(allDF)
 maxN=0
@@ -113,17 +140,34 @@ allData = allDF.values
 
 uid = allDF['clip'].values*10000 + allDF['c_id'].values 
 
-mvector = allDF['dtheta'].values
+np.save('neighbours.npy', neighbours)
+np.save('uid.npy', uid)
+
+
+mvector = allDF['move1'].values
 mvector[mvector<-pi]=mvector[mvector<-pi]+2*pi
 mvector[mvector>pi]=mvector[mvector>pi]-2*pi
+np.save('mvector1.npy', mvector)
+
+mvector = allDF['move2'].values
+mvector[mvector<-pi]=mvector[mvector<-pi]+2*pi
+mvector[mvector>pi]=mvector[mvector>pi]-2*pi
+np.save('mvector2.npy', mvector)
+
+mvector = allDF['move5'].values
+mvector[mvector<-pi]=mvector[mvector<-pi]+2*pi
+mvector[mvector>pi]=mvector[mvector>pi]-2*pi
+np.save('mvector5.npy', mvector)
+
+mvector = allDF['move10'].values
+mvector[mvector<-pi]=mvector[mvector<-pi]+2*pi
+mvector[mvector>pi]=mvector[mvector>pi]-2*pi
+np.save('mvector10.npy', mvector)
+
 
 evector = allDF['env_heading'].values
 evector[evector<-pi]=evector[evector<-pi]+2*pi
 evector[evector>pi]=evector[evector>pi]-2*pi
-
-np.save('neighbours.npy', neighbours)
-np.save('mvector.npy', mvector)
 np.save('evector.npy', evector)
-np.save('uid.npy', uid)
     
 
