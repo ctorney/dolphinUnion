1524032e423ba90d0942e3b3e40e7c923a13a6ed
Colin Torney <colin.j.torney@gmail.com>
2016-04-29 16:21:10 +0100

    2-sec moves only

--- analysis/movement/preprocessData.py
+++ analysis/movement/preprocessData.py
@@ -33,12 +33,7 @@ for indexF, rowF in df.iterrows():
     posDF['clip']=indexF
     posDF = posDF[posDF['frame']%120==0]
     dt = 60 # 60 frames is 1 second
-    posDF['move1']=np.NaN
-    posDF['move2']=np.NaN
-    posDF['move3']=np.NaN
-    posDF['move4']=np.NaN
-    posDF['move5']=np.NaN
-    posDF['move10']=np.NaN
+    posDF['move']=np.NaN
     posDF['env_heading']=np.NaN
     for index, row in posDF.iterrows():
         thisFrame =  row['frame']
@@ -48,7 +43,7 @@ for indexF, rowF in df.iterrows():
             
         thisTheta = row['heading']
         # calculate the change in heading from this point to the next
-        nextTime = posDF[(np.abs(posDF['frame']-(thisFrame+dt))<1e-6)&(posDF['c_id']==thisID)]
+        nextTime = posDF[(np.abs(posDF['frame']-(thisFrame+2*dt))<1e-6)&(posDF['c_id']==thisID)]
         if len(nextTime)==1:
             # calculate the average heading all the other caribou were taking at this point
             excThis = posDF[posDF.c_id!=thisID]
@@ -56,60 +51,29 @@ for indexF, rowF in df.iterrows():
             yp = excThis['y'].values
             xdirs = np.cos(excThis['heading'].values)
             ydirs = np.sin(excThis['heading'].values)
-            kappa = 2.0**2
+            # decay rate of 3 gives the maximum likelihood for the environment only model
+            kappa = 3.0**2
             dists = (((xp - thisX)**2 + (yp - thisY)**2))
             weights = np.exp(-dists/kappa)
-            xav = np.sum(weights*xdirs)/np.sum(weights)
-            yav = np.sum(weights*ydirs)/np.sum(weights)
-            posDF.ix[index,'env_heading']  = math.atan2(yav,xav)-  thisTheta
+            if np.sum(weights)>0:
+                xav = np.sum(weights*xdirs)/np.sum(weights)
+                yav = np.sum(weights*ydirs)/np.sum(weights)
+                posDF.ix[index,'env_heading']  = math.atan2(yav,xav)-  thisTheta
+            else:
+                posDF.ix[index,'env_heading']  = 0.0
             
             # 1 second move heading
             dx = nextTime.iloc[0]['x'] - thisX
             dy = nextTime.iloc[0]['y'] - thisY
-            posDF.ix[index,'move1'] = math.atan2(dy,dx) -  thisTheta
+            posDF.ix[index,'move'] = math.atan2(dy,dx) -  thisTheta
 
-        nextTime = posDF[(np.abs(posDF['frame']-(thisFrame+2*dt))<1e-6)&(posDF['c_id']==thisID)]
-        if len(nextTime)==1:
-            # 2 second move heading
-            dx = nextTime.iloc[0]['x'] - thisX
-            dy = nextTime.iloc[0]['y'] - thisY
-            posDF.ix[index,'move2'] = math.atan2(dy,dx) -  thisTheta
-    
-        nextTime = posDF[(np.abs(posDF['frame']-(thisFrame+3*dt))<1e-6)&(posDF['c_id']==thisID)]
-        if len(nextTime)==1:
-            # 2 second move heading
-            dx = nextTime.iloc[0]['x'] - thisX
-            dy = nextTime.iloc[0]['y'] - thisY
-            posDF.ix[index,'move3'] = math.atan2(dy,dx) -  thisTheta
-            
-        nextTime = posDF[(np.abs(posDF['frame']-(thisFrame+4*dt))<1e-6)&(posDF['c_id']==thisID)]
-        if len(nextTime)==1:
-            # 2 second move heading
-            dx = nextTime.iloc[0]['x'] - thisX
-            dy = nextTime.iloc[0]['y'] - thisY
-            posDF.ix[index,'move4'] = math.atan2(dy,dx) -  thisTheta
-            
-        nextTime = posDF[(np.abs(posDF['frame']-(thisFrame+5*dt))<1e-6)&(posDF['c_id']==thisID)]
-        if len(nextTime)==1:
-            # 5 second move heading
-            dx = nextTime.iloc[0]['x'] - thisX
-            dy = nextTime.iloc[0]['y'] - thisY
-            posDF.ix[index,'move5'] = math.atan2(dy,dx) -  thisTheta
-        
-        nextTime = posDF[(np.abs(posDF['frame']-(thisFrame+10*dt))<1e-6)&(posDF['c_id']==thisID)]
-        if len(nextTime)==1:
-            # 10 second move heading
-            dx = nextTime.iloc[0]['x'] - thisX
-            dy = nextTime.iloc[0]['y'] - thisY
-            posDF.ix[index,'move10'] = math.atan2(dy,dx) -  thisTheta
-        
             
 
 
     allDF = allDF.append(posDF,ignore_index=True)
 
     
-allDF = allDF[np.isfinite(allDF['move2'])]
+allDF = allDF[np.isfinite(allDF['move'])]
 allDF = allDF.reset_index(drop=True)
 dsize = len(allDF)
 maxN=0
@@ -161,36 +125,11 @@ uid = allDF['clip'].values*10000 + allDF['c_id'].values
 np.save('pdata/neighbours.npy', neighbours)
 np.save('pdata/uid.npy', uid)
 
-
-mvector = allDF['move1'].values
+mvector = allDF['move'].values
 mvector[mvector<-pi]=mvector[mvector<-pi]+2*pi
 mvector[mvector>pi]=mvector[mvector>pi]-2*pi
-np.save('pdata/mvector1.npy', mvector)
+np.save('pdata/mvector.npy', mvector)
 
-mvector = allDF['move2'].values
-mvector[mvector<-pi]=mvector[mvector<-pi]+2*pi
-mvector[mvector>pi]=mvector[mvector>pi]-2*pi
-np.save('pdata/mvector2.npy', mvector)
-
-mvector = allDF['move3'].values
-mvector[mvector<-pi]=mvector[mvector<-pi]+2*pi
-mvector[mvector>pi]=mvector[mvector>pi]-2*pi
-np.save('pdata/mvector3.npy', mvector)
-
-mvector = allDF['move4'].values
-mvector[mvector<-pi]=mvector[mvector<-pi]+2*pi
-mvector[mvector>pi]=mvector[mvector>pi]-2*pi
-np.save('pdata/mvector4.npy', mvector)
-
-mvector = allDF['move5'].values
-mvector[mvector<-pi]=mvector[mvector<-pi]+2*pi
-mvector[mvector>pi]=mvector[mvector>pi]-2*pi
-np.save('pdata/mvector5.npy', mvector)
-
-mvector = allDF['move10'].values
-mvector[mvector<-pi]=mvector[mvector<-pi]+2*pi
-mvector[mvector>pi]=mvector[mvector>pi]-2*pi
-np.save('pdata/mvector10.npy', mvector)
 
 
 evector = allDF['env_heading'].values
