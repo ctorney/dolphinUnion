03afe78d1dde67a0bbbee6bd29b8ca204cb62bc3
Colin Torney <colin.j.torney@gmail.com>
2016-05-12 11:16:54 +0100

    constant model parameters

--- analysis/movement/variation/socialForce.py
+++ analysis/movement/variation/socialForce.py
@@ -15,14 +15,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-at_l = 6.6710
-at_a=0.2111
-al_w=0.1350
-social=0.9657
-al=0.4773 
-be=0.136 
-rm = 0.92
-re = 0.93
+at_l = 6.6208
+at_a=0.1852
+al_w=0.5582
+social=0.9645
+al=0.5582 
+be=0.135 
+rm = 0.921
+re = 0.930
+ig=0.2866
 
 
 
@@ -34,22 +35,33 @@ mvector = np.load('../pdata/mvector.npy')
 evector = np.load('../pdata/evector.npy')
 uid = np.load('../pdata/uid.npy')
 
+dists = neighbours[:,:,4]
+stepLen=np.mean(dists[dists>0])
 
-    
+inds = np.unique(uid)
+
+for i,thisID in enumerate(inds):
+    if len(uid[uid==thisID])<15:
+        neighbours = neighbours[uid!=thisID]
+        mvector = mvector[uid!=thisID]
+        evector = evector[uid!=thisID]
+        uid = uid[uid!=thisID]
+
+
+xj = (neighbours[:,:,0]*np.cos(neighbours[:,:,1]))+(np.cos(neighbours[:,:,3])*(al_w/stepLen)*neighbours[:,:,4])
+yj = (neighbours[:,:,0]*np.sin(neighbours[:,:,1]))+(np.sin(neighbours[:,:,3])*(al_w/stepLen)*neighbours[:,:,4])
+
+anglesj=np.arctan2(yj,xj)
+
 n_weights = np.ones_like(neighbours[:,:,0],dtype=np.float64)
-n_weights[neighbours[:,:,0]==0]=0.0
+n_weights[neighbours[:,:,0]<=ig]=0.0
 n_weights[neighbours[:,:,0]>at_l]=0.0
 n_weights[(neighbours[:,:,1]<-at_a)|(neighbours[:,:,1]>at_a)]=0.0
 
-#   na_weights = al_w*np.ones_like(neighbours[:,:,0],dtype=np.float64)
-#   na_weights[neighbours[:,:,0]==0]=0.0
-#   na_weights[neighbours[:,:,0]>al_l]=0.0
-#   na_weights[(neighbours[:,:,1]<-al_a)|(neighbours[:,:,1]>al_a)]=0.0
+xsv = np.sum(np.cos(anglesj)*n_weights,1)
+ysv = np.sum(np.sin(anglesj)*n_weights,1)
 
-#xsv = np.sum(np.cos(neighbours[:,:,1])*n_weights,1) + np.sum(np.cos(neighbours[:,:,3])*na_weights,1)
-#   ysv = np.sum(np.sin(neighbours[:,:,1])*n_weights,1) + np.sum(np.sin(neighbours[:,:,3])*na_weights,1)
-xsv = np.sum((np.cos(neighbours[:,:,1])+np.cos(neighbours[:,:,3])*al_w) *n_weights,1)# + np.sum(np.cos(neighbours[:,:,3])*na_weights,1)
-ysv = np.sum((np.sin(neighbours[:,:,1])+np.sin(neighbours[:,:,3])*al_w) *n_weights,1)#ysv
+    
 
 
 
@@ -90,7 +102,7 @@ minswcc = al*wcs + (1.0-al)*(be*wce+(1.0-be)*wcm)
 
 
 #social force is how much do we reduce uncertainty if we include the social vector
-sf = np.log(wcc) - np.log(asocwcc)
+sf = (np.log(wcc) - np.log(asocwcc))
 maxSF = np.log(maxswcc) - np.log(asocwcc)
 minSF = np.log(minswcc) - np.log(asocwcc)
 
@@ -113,8 +125,8 @@ for i,thisID in enumerate(inds):
 x=np.argsort(means)
 means = means[x]
 stddevs = stddevs[x]
-stddevs=stddevs[means!=0]
-means=means[means!=0]
+#stddevs=stddevs[means!=0]
+#means=means[means!=0]
 inds = inds[x]
 
 sfSORT = np.array([])
@@ -133,8 +145,8 @@ for i,thisID in enumerate(inds):
 
 
 plt.plot(means,'r')
-#plt.plot(means+stddevs,'b')
-#plt.plot(means-stddevs,'b')
+plt.plot(means+stddevs,'b')
+plt.plot(means-stddevs,'b')
 #plt.figure()
 plt.plot(iSORT,sfSORT,'.')
 empties = np.zeros_like(inds)
@@ -142,17 +154,17 @@ for i,thisID in enumerate(inds):
     if np.isnan(means[i]):
         continue
     thisSocial=sv[uid==thisID]
-    empties[i]=len(thisSocial[(thisSocial[:,1]==0)&(thisSocial[:,0]==0)])/len(thisSocial)
+    empties[i]=len(thisSocial[(thisSocial[:,1]==0)&(thisSocial[:,0]==0)])#/len(thisSocial)
 empties = empties[np.isfinite(means)]
 plt.plot(empties,'.')
 
 
 
-[counts,_,_] = np.histogram2d(sfSORT,iSORT,bins=40)
+[counts,_,_] = np.histogram2d(sfSORT,iSORT,bins=20)
 counts = counts/np.sum(counts,0)
 plt.figure()
-plt.pcolormesh(counts,vmin=0,vmax=0.15,cmap='bone')#,lw=0.0,vmin=np.min(hista2),vmax=np.max(hista2),cmap='viridis')
-uid = np.load('../pdata/uid.npy')
+plt.pcolormesh(counts,vmin=0.05,vmax=0.2,cmap='bone')#,lw=0.0,vmin=np.min(hista2),vmax=np.max(hista2),cmap='viridis')
+
 
 
 
