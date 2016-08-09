b71aefb5528583aff4ce336e7f47c60139d18001
Colin Torney <colin.j.torney@gmail.com>
2016-08-09 10:55:14 +0100

    angle changes

--- analysis/movement/likelihood_check/differences.py
+++ analysis/movement/likelihood_check/differences.py
@@ -6,11 +6,11 @@ import scipy.stats
 import constantModelAlign
 import decayModelAlign
 import networkModelAlign
+plt.close('all')
 
-
-#cma = constantModelAlign.constantP()
-#dma = decayModelAlign.decayP()
-#nma = networkModelAlign.networkP()
+cma = constantModelAlign.constantP()
+dma = decayModelAlign.decayP()
+nma = networkModelAlign.networkP()
 
 #plt.plot(cma,dma,'.')
 #plt.figure()
@@ -37,33 +37,59 @@ metrics2 = np.zeros_like(mvector)-1
 for i,db in enumerate(neighbours):
     rowLoc = np.zeros((0,1)).astype(np.float32)
     for row in db:
-        if row[0]>0 and row[0]<10.0 and row[1]>-0.3 and row[1]<0.3:
+        if row[0]>0 and row[0]<10.0 and row[1]>-0.2518 and row[1]<0.2518:
             
             rowLoc = np.vstack((rowLoc,row[0]))
     if len(rowLoc):
-        metrics1[i]=np.min(rowLoc)#-np.min(rowLoc)
+        metrics2[i]=np.min(rowLoc)#-np.min(rowLoc)
     if len(rowLoc)>1:
         sndmin = np.partition(np.reshape(rowLoc,(len(rowLoc))),1)[1]
-        metrics2[i]=sndmin-np.min(rowLoc)
+        metrics1[i]=sndmin-np.min(rowLoc)
         
 
     
 like_diff=dma-cma
 like_diff1=like_diff[metrics1>=0]
-metrics1=metrics1[metrics1>=0]
+#metrics1=metrics1[metrics1>=0]
 like_diff2=like_diff[metrics2>=0]
-metrics2=metrics2[metrics2>=0]
+#metrics2=metrics2[metrics2>=0]
 
 
 rmin=0
-rmax=8
-nbins=8
-bin_means, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,like_diff1,   statistic='mean', bins=nbins, range=[rmin,rmax])
+rmax=10
+nbins=10
+bin_means, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,dma,   statistic='mean', bins=nbins, range=[rmin,rmax])
 
 
 
-err, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,like_diff1,   statistic=np.std, bins=nbins, range=[rmin,rmax])
-counts, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,like_diff1,   statistic='count', bins=nbins,range=[rmin,rmax])
+err, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,dma,   statistic=np.std, bins=nbins, range=[rmin,rmax])
+counts, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,dma,   statistic='count', bins=nbins,range=[rmin,rmax])
+bin_centres = 0.5*(bin_edges[:-1]+bin_edges[1:])
+err/=np.sqrt(counts)
+
+
+plt.errorbar(bin_centres, bin_means, yerr=err, fmt='-o')
+
+
+bin_means, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,cma,   statistic='mean', bins=nbins, range=[rmin,rmax])
+
+
+
+err, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,cma,   statistic=np.std, bins=nbins, range=[rmin,rmax])
+counts, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,cma,   statistic='count', bins=nbins,range=[rmin,rmax])
+bin_centres = 0.5*(bin_edges[:-1]+bin_edges[1:])
+err/=np.sqrt(counts)
+
+
+plt.errorbar(bin_centres, bin_means, yerr=err, fmt='-o')
+
+
+bin_means, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,nma,   statistic='mean', bins=nbins, range=[rmin,rmax])
+
+
+
+err, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,nma,   statistic=np.std, bins=nbins, range=[rmin,rmax])
+counts, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,nma,   statistic='count', bins=nbins,range=[rmin,rmax])
 bin_centres = 0.5*(bin_edges[:-1]+bin_edges[1:])
 err/=np.sqrt(counts)
 
@@ -71,21 +97,21 @@ err/=np.sqrt(counts)
 plt.errorbar(bin_centres, bin_means, yerr=err, fmt='-o')
 #plt.figure()
 
-bin_means, bin_edges, binnumber = scipy.stats.binned_statistic(metrics2,like_diff2,   statistic='mean', bins=nbins,range=[rmin,rmax])#, range=[0,0.1])
-
-err, bin_edges, binnumber = scipy.stats.binned_statistic(metrics2,like_diff2,   statistic=np.std, bins=nbins,range=[rmin,rmax])#, range=[0,0.1])
-counts, bin_edges, binnumber = scipy.stats.binned_statistic(metrics2,like_diff2,   statistic='count', bins=nbins,range=[rmin,rmax])#, range=[0,0.1])
-bin_centres = 0.5*(bin_edges[:-1]+bin_edges[1:])
-err/=np.sqrt(counts)
-
-
-plt.errorbar(bin_centres, bin_means, yerr=err, fmt='-o')
-
-plt.ylim([-0.08,0.08])
-plt.axhline(y=0, color='k')
-
-
-plt.figure()
+#bin_means, bin_edges, binnumber = scipy.stats.binned_statistic(metrics2,like_diff2,   statistic='mean', bins=nbins,range=[rmin,rmax])#, range=[0,0.1])
+#
+#err, bin_edges, binnumber = scipy.stats.binned_statistic(metrics2,like_diff2,   statistic=np.std, bins=nbins,range=[rmin,rmax])#, range=[0,0.1])
+#counts, bin_edges, binnumber = scipy.stats.binned_statistic(metrics2,like_diff2,   statistic='count', bins=nbins,range=[rmin,rmax])#, range=[0,0.1])
+#bin_centres = 0.5*(bin_edges[:-1]+bin_edges[1:])
+#err/=np.sqrt(counts)
+#
+#
+#plt.errorbar(bin_centres, bin_means, yerr=err, fmt='-o')
+#
+##plt.ylim([-0.08,0.08])
+#plt.axhline(y=0, color='k')
+#
+#
+#plt.figure()
 metrics1 = np.zeros_like(mvector)-1
 
 
@@ -93,7 +119,7 @@ for i,db in enumerate(neighbours):
     rowLoc = np.zeros((0)).astype(np.float32)
     rowAng = np.zeros((0)).astype(np.float32)
     for row in db:
-        if row[0]>0 and row[0]<10.0 and row[1]>-0.3 and row[1]<0.3:
+        if row[0]>0 and row[0]<10.0 and row[1]>-0.2518 and row[1]<0.2518:
             
             rowLoc = np.append(rowLoc,row[0])
 
@@ -103,29 +129,53 @@ for i,db in enumerate(neighbours):
         
     if len(rowLoc)>1:
         fmin = np.min(rowLoc)#-np.min(rowLoc)
-        #sndmin = np.partition(np.reshape(rowLoc,(len(rowLoc))),1)[1]
+        sndmin = np.partition(np.reshape(rowLoc,(len(rowLoc))),1)[1]
         i1 = np.argwhere(rowLoc==fmin)
-        #i2 = np.argwhere(rowLoc==sndmin)
+        i2 = np.argwhere(rowLoc==sndmin)
         
-        #metrics1[i] = abs(i1-np.mean(rowAng[np.arange(len(rowAng))!=i1[0][0]]))#len(rowLoc)# np.max(abs(rowAng[i1]-rowAng))
-        metrics1[i] = abs(i1-np.mean(rowAng))
+        #metrics1[i] = len(rowLoc)#abs(rowAng[i1]-np.mean(rowAng[np.arange(len(rowAng))!=i1[0][0]]))#len(rowLoc)# np.max(abs(rowAng[i1]-rowAng))
+        metrics1[i] = abs(rowAng[i1[0][0]]) - abs(np.mean(rowAng[np.arange(len(rowAng))!=i1[0][0]]))#rowAng[i2[0][0]])#abs(np.max(rowAng)-np.min(rowAng)) #np.std(rowAng)#abs(rowAng[i1]-np.mean(rowAng))
         
 
     
 like_diff=dma-nma
 like_diff1=like_diff[metrics1>=0]
-metrics1=metrics1[metrics1>=0]
+#metrics1=metrics1[metrics1>=0]
 
-
-rmin=0.0
-rmax=0.25
+plt.figure()
+rmin=-0.2
+rmax=0.2
 nbins=10
-bin_means, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,like_diff1,   statistic='mean', bins=nbins, range=[rmin,rmax])
+bin_means, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,like_diff,   statistic='mean', bins=nbins, range=[rmin,rmax])
 
 
 
-err, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,like_diff1,   statistic=np.std, bins=nbins, range=[rmin,rmax])
-counts, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,like_diff1,   statistic='count', bins=nbins,range=[rmin,rmax])
+err, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,like_diff,   statistic=np.std, bins=nbins, range=[rmin,rmax])
+counts, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,like_diff,   statistic='count', bins=nbins,range=[rmin,rmax])
+bin_centres = 0.5*(bin_edges[:-1]+bin_edges[1:])
+err/=np.sqrt(counts)
+
+
+plt.errorbar(bin_centres, bin_means, yerr=err, fmt='-o')
+
+bin_means, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,nma,   statistic='mean', bins=nbins, range=[rmin,rmax])
+
+
+
+err, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,nma,   statistic=np.std, bins=nbins, range=[rmin,rmax])
+counts, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,nma,   statistic='count', bins=nbins,range=[rmin,rmax])
+bin_centres = 0.5*(bin_edges[:-1]+bin_edges[1:])
+err/=np.sqrt(counts)
+
+
+plt.errorbar(bin_centres, bin_means, yerr=err, fmt='-o')
+
+bin_means, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,cma,   statistic='mean', bins=nbins, range=[rmin,rmax])
+
+
+
+err, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,cma,   statistic=np.std, bins=nbins, range=[rmin,rmax])
+counts, bin_edges, binnumber = scipy.stats.binned_statistic(metrics1,cma,   statistic='count', bins=nbins,range=[rmin,rmax])
 bin_centres = 0.5*(bin_edges[:-1]+bin_edges[1:])
 err/=np.sqrt(counts)
 
