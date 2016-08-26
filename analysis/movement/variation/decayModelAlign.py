03c0c3ef6e2b8041ca67da4515dc0a49bc968181
Colin Torney <colin.j.torney@gmail.com>
2016-08-26 16:36:40 +0100

    classes

--- analysis/movement/variation/decayModelAlign.py
+++ analysis/movement/variation/decayModelAlign.py
@@ -16,17 +16,17 @@ import matplotlib.pyplot as plt
 __all__ = ['ignore_length','attract_exponent','attract_length','attract_angle','align_weight','rho_s','rho_m','rho_e','alpha','beta','mvector']
 
 
-align_weight = Uniform('align_weight', lower=0.0, upper=2.0,value=0.77)
-ignore_length = Uniform('ignore_length', lower=0.0, upper=5.0,value=0.250)
-attract_length = Uniform('attract_length', lower=0.5, upper=20.0,value=3.175)
-attract_exponent = Uniform('attract_exponent', lower=0.0, upper=5.0,value=2.36)
-attract_angle = Uniform('attract_angle', lower=0, upper=pi,value=0.189)
-rho_s = Uniform('rho_s',lower=0, upper=1,value=0.967)
-alpha = Uniform('alpha',lower=0, upper=1,value=0.37)
+ignore_length = Uniform('ignore_length', lower=0.0, upper=5.0,value=1.33)
+attract_length = Uniform('attract_length', lower=0.5, upper=20.0,value=5.00)
+attract_angle = Uniform('attract_angle', lower=0, upper=pi,value=0.276)
+align_weight = Uniform('align_weight', lower=0.0, upper=2.0,value=0.767)
+rho_s = Uniform('rho_s',lower=0, upper=1,value=0.940)
+alpha = Uniform('alpha',lower=0, upper=1,value=0.384)
+beta = Uniform('beta',lower=0, upper=1,value=0.129)
 
-rho_m = 0.937
-rho_e = 0.956
-beta = 0.126
+attract_exponent = Uniform('attract_exponent', lower=1.0, upper=25.0,value=9.1)
+
+
 
 neighbours = np.load('../pdata/neighbours.npy')
 mvector = np.load('../pdata/mvector.npy')
@@ -50,12 +50,19 @@ evector=evector[indexList]
  
 @deterministic(plot=False)
 def social_vector(at_l=attract_length, at_a=attract_angle, at_de=attract_exponent, al_w=align_weight, ig=ignore_length):
+    if at_l<0.5:
+        at_l=0.5
 
-    xj = (neighbours[:,:,0]*np.cos(neighbours[:,:,1]))+(np.cos(neighbours[:,:,3])*(al_w/stepLen)*neighbours[:,:,4])
-    yj = (neighbours[:,:,0]*np.sin(neighbours[:,:,1]))+(np.sin(neighbours[:,:,3])*(al_w/stepLen)*neighbours[:,:,4])
+#xj = (neighbours[:,:,0]*np.cos(neighbours[:,:,1]))+(np.cos(neighbours[:,:,3])*(al_w/stepLen)*neighbours[:,:,4])
+#    yj = (neighbours[:,:,0]*np.sin(neighbours[:,:,1]))+(np.sin(neighbours[:,:,3])*(al_w/stepLen)*neighbours[:,:,4])
+#xj = (1.0-al_w)*np.cos(neighbours[:,:,1])+(np.cos(neighbours[:,:,3])*al_w)
+#   yj = (1.0-al_w)*np.sin(neighbours[:,:,1])+(np.sin(neighbours[:,:,3])*al_w)
+    xj = (neighbours[:,:,0]*np.cos(neighbours[:,:,1]))+(np.cos(neighbours[:,:,3])*(al_w))#/stepLen)*neighbours[:,:,4])
+    yj = (neighbours[:,:,0]*np.sin(neighbours[:,:,1]))+(np.sin(neighbours[:,:,3])*(al_w))#/stepLen)*neighbours[:,:,4])
+        
         
     anglesj=np.arctan2(yj,xj)
-    n_weights = np.exp(-(neighbours[:,:,0]/at_l)**at_de)
+    n_weights = np.exp(-((neighbours[:,:,0]/at_l)**at_de))
     n_weights[neighbours[:,:,0]<=ig]=0.0
     n_weights[(neighbours[:,:,1]<-at_a)|(neighbours[:,:,1]>at_a)]=0.0
 
@@ -66,28 +73,23 @@ def social_vector(at_l=attract_length, at_a=attract_angle, at_de=attract_exponen
 
     out[:,0] = xsv
     out[:,1] = ysv
-    out[(xsv==0)&(ysv==0),0]=1.0
-    out[(xsv==0)&(ysv==0),1]=0.0
     
     return out
 
 
 @stochastic(observed=True)
-def moves(social=rho_s, al=alpha, sv=social_vector, value=mvector):
-    rm=rho_m
-    re=rho_e
-    be=beta
+def moves(social=rho_s, al=alpha, be=beta, sv=social_vector, value=mvector):
     # this is the main function that calculates the log probability of all the moves based on the parameters that are passed in
     # and the assumed interaction function
     svv = np.arctan2(sv[:,1],sv[:,0])
     als = al*np.ones_like(svv)
     als[(sv[:,1]==0)&(sv[:,0]==0)]=0
+    xvals = als*np.cos(svv) + (1.0-als)*(be*np.cos(evector)+(1.0-be))
+    yvals = als*np.sin(svv) + (1.0-als)*(be*np.sin(evector))
+
+    allV = np.arctan2(yvals,xvals)
     
-    wcs = (1/(2*pi)) * (1-(social*social))/(1+(social*social)-2*social*np.cos((svv-mvector).transpose()))
-    wce = (1/(2*pi)) * (1-(re*re))/(1+(re*re)-2*re*np.cos((evector-mvector).transpose())) # weighted wrapped cauchy
-    wcm = (1/(2*pi)) * (1-(rm*rm))/(1+(rm*rm)-2*rm*np.cos((-mvector).transpose())) # weighted wrapped cauchy
+    wcs = (1/(2*pi)) * (1-(social*social))/(1+(social*social)-2*social*np.cos((allV-mvector).transpose()))
 
-    wcc = als*wcs + (1.0-als)*(be*wce+(1.0-be)*wcm)
-    wcc = wcc[wcc>0]
-    return np.sum(np.log(wcc))
-
+    wcs = wcs[wcs>0]
+    return np.sum(np.log(wcs))
--- analysis/movement/variation/runClasses.py
+++ analysis/movement/variation/runClasses.py
@@ -5,46 +5,48 @@ from pymc import MCMC
 import numpy as np
 import matplotlib.pyplot as plt
 import decayModelAlign
-import fixedSocialCls
+#import fixedSocialCls
 
 
-inds = np.unique(uid)
 Ni = 3
 
-iters = 50000
-burns = 25000
-thins = 1
+iters = 400000
+burns = 50000
+thins = 10
 Irhos = np.empty((Ni,int((iters-burns)/thins)))
 Ialphas = np.empty((Ni,int((iters-burns)/thins)))
+Ibetas = np.empty((Ni,int((iters-burns)/thins)))
 Iig = np.empty((Ni,int((iters-burns)/thins)))
 Iil = np.empty((Ni,int((iters-burns)/thins)))
 Iia = np.empty((Ni,int((iters-burns)/thins)))
 Iiw = np.empty((Ni,int((iters-burns)/thins)))
 Iae = np.empty((Ni,int((iters-burns)/thins)))
-for i in range(Ni):
+for i in range(0, 1):
 
     print('-------------')
-    print('Processing individual ' + str(i+1) + ' out of ' + str(Ni))
+    print('Processing class ' + str(i+1) + ' out of ' + str(Ni))
     print('-------------')
     np.save('terribleHackClass.npy',np.array([i]))
-    imp.reload(fixedSocialCls)
-    M = MCMC(fixedSocialCls)
+    imp.reload(decayModelAlign)
+    M = MCMC(decayModelAlign)
     M.sample(iter=iters, burn=burns, thin=thins,verbose=0)
-#    Irhos[i,:]=M.trace('rho_s')[:]
+    Irhos[i,:]=M.trace('rho_s')[:]
     Ialphas[i,:]=M.trace('alpha')[:]
-#    Iig[i,:]=M.trace('ignore_length')[:]
-#    Iil[i,:]=M.trace('attract_length')[:]
-#    Iia[i,:]=M.trace('attract_angle')[:]
-#    Iiw[i,:]=M.trace('align_weight')[:]
-#    Iae[i,:]=M.trace('attract_exponent')[:]
+    Ibetas[i,:]=M.trace('beta')[:]
+    Iig[i,:]=M.trace('ignore_length')[:]
+    Iil[i,:]=M.trace('attract_length')[:]
+    Iia[i,:]=M.trace('attract_angle')[:]
+    Iiw[i,:]=M.trace('align_weight')[:]
+    Iae[i,:]=M.trace('attract_exponent')[:]
     
-#np.save('Irhos.npy',Irhos)
-np.save('Ialphas.npy',Ialphas)
-#np.save('Iig.npy',Iig)
-#np.save('Iil.npy',Iil)
-#np.save('Iia.npy',Iia)
-#np.save('Iiw.npy',Iiw)
-#np.save('Iae.npy',Iae)
+np.save('Irhos0.npy',Irhos)
+np.save('Ialphas0.npy',Ialphas)
+np.save('Ibetas0.npy',Ibetas)
+np.save('Iig0.npy',Iig)
+np.save('Iil0.npy',Iil)
+np.save('Iia0.npy',Iia)
+np.save('Iiw0.npy',Iiw)
+np.save('Iae0.npy',Iae)
 #
 #np.save('mc_data/cma_at_a.npy',M.trace('attract_angle')[:])
 #np.save('mc_data/cma_at_l.npy',M.trace('attract_length')[:])
