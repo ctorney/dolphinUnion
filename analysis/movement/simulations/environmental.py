774d94d6137a7c6b92f1a2d5e73c2401063cb7d6
Colin Torney <colin.j.torney@gmail.com>
2017-08-17 10:47:13 +0100

    replaced sine wave with random OU process environment

--- analysis/movement/simulations/environmental.py
+++ analysis/movement/simulations/environmental.py
@@ -1,124 +1,117 @@
+
 import numpy as np
 from math import pi, sin, cos, atan2
 from math import *
 import math
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy.interpolate import interp1d
 
 
-outTracks = pd.DataFrame(columns= ['frame','x','y','heading','c_id'])
 
-trackName = 'environmental.csv'
+outTracks = pd.DataFrame(columns= ['frame','x','y','heading','c_id','clip'])
+
+trackName = 'environment/data_sim.csv'
 
 
-XDIM = 100
+clip = 0
+
+for clip in range(10):    
+    print(clip)
+    # number of individuals
+    N=10
     
-# number of individuals
-N=50
-
-# set to random initial conditions on (0,1)x(0,1)
-
-xspace = 2
-xpos = np.arange(5,5+xspace*N,xspace)
-#xpos = np.random.uniform(5,15,N)
-ypos = np.random.uniform(48,52,N)
-
-# set to random inital headings
-heading = np.random.uniform(0,2*pi,N)
-
-randvar = 0.1
-
-# set speed individuals move
-speed = 1
-
-# run for this many time steps
-TIMESTEPS = 7000
-
-mvp = 3 # mean reversion parameter
-mean_heading = 0;
-sig =0.5 # noise
-dt = 1e-2 # time step
-#t = np.arange(0,dt*2,dt) #             % Time vector
-#x0 = 0;                 #% Set initial condition
-##rng(1);                 #% Set random seed
-#W = np.zeros((len(t))); #% Allocate integrated W vector
-#np.random.seed(0)
-#for i in range(len(t)-1):
-#    W[i+1] = sqrt(exp(2*th*dt)-1)*np.random.normal()
-#
-#ex = np.exp(-th*t);
-#x = x0*ex+mu*(1-ex)+sig*ex*W/sqrt(2*th);
-#
-#np.random.seed(0)
-
-exp_mr = math.exp(-mvp*dt)
-add_noise = sig*math.sqrt((math.exp(2*mvp*dt)-1)/(2*mvp))
-
-repRad = 0.1
-socWeight = 2.5
-def getHeading(i):
     
-    yc = XDIM/2 + 0.5*(np.sin(0.5*xpos[i])+ np.sin(2.4*xpos[i])+ np.sin(1.3*xpos[i])+ np.sin(3.1*xpos[i]))
-    dy = yc - ypos[i]
-    dx = 0.05
-    angle = atan2(dy,dx)
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # first we create a stochastic environment
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    mvp = 1 # mean reversion parameter
+    sig = 0.5 # noise
+    dt = 1e-3 # time step
+    exp_mr = math.exp(-mvp*dt)
+    add_noise = sig*math.sqrt((math.exp(2*mvp*dt)-1)/(2*mvp))
     
-#    socx = 0
-#    socy = 0
-#    for j in range(N):
-#        if i==j:
-#            continue
-#        distij = ((xpos[i]-xpos[j])**2+ (ypos[i]-ypos[j])**2)**0.5
-#        if distij < repRad:
-#            thisAngle = math.atan2(ypos[i]-ypos[j],xpos[i]-xpos[j])
-#            socx = socx + cos(thisAngle)
-#            socy = socy + sin(thisAngle)
-#    
-#    if socx!=0 and socy!=0:
-#        soc_angle = atan2(socy,socx)
-#        angle = atan2(sin(angle) + socWeight*sin(soc_angle),cos(angle)+socWeight*cos(soc_angle))
+    dx=0.1
+    xspace = 100
+    
+    xn = int(xspace/dx)
+    
+    randField = np.zeros(xn)
+    xvals = np.arange(0,xspace,dx)
+    
+    for x in range(1,xn):
+        new_rand = randField[x-1]-0
+        randField[x] = 0 + exp_mr*(new_rand+add_noise*np.random.normal())        
+    
+    sm = 25
+    ma = np.ones(sm)/sm
+    randField = randField*5/np.max(randField)
+    randField = np.convolve(randField, ma, mode='same')
         
-        
-    return math.atan2(math.sin(angle),math.cos(angle))
-
-
-# simulate individual movement
-for t in range(TIMESTEPS):
     
-    print(t,TIMESTEPS)
     
+    plt.figure()
+    plt.plot(xvals,randField)
+    
+    # this creates the interpolation function
+    f_interp = interp1d(xvals, randField, kind='cubic')
+    
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # put all individuals on the trail
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    xpos = np.random.uniform(0,10,N)
+    ypos = np.zeros_like(xpos)
+    heading = np.zeros_like(xpos)
     for i in range(N):
-        mean_heading = getHeading(i)
-        new_angle = math.atan2(math.sin(heading[i]-mean_heading),math.cos(heading[i]-mean_heading))
-        new_angle = mean_heading + exp_mr*(new_angle+add_noise*np.random.normal())
-        heading[i] =math.atan2(math.sin(new_angle),math.cos(new_angle))
-    #heading = heading + np.random.normal(0,randvar,N)
+        ypos[i] = f_interp(xpos[i])
+        
+    plt.plot(xpos,ypos,'.')
     
-    # individuals move in direction defined by heading with fixed speed
-    xpos = xpos + dt*speed*np.cos(heading)
-    ypos = ypos + dt*speed*np.sin(heading)
-    # boundary conditions are periodic
-    xpos[xpos<0]=xpos[xpos<0]+XDIM
-    xpos[xpos>XDIM]=xpos[xpos>XDIM]-XDIM
-    ypos[ypos<0]=ypos[ypos<0]+XDIM
-    ypos[ypos>XDIM]=ypos[ypos>XDIM]-XDIM
+    # run for this many time steps
+    TIMESTEPS = 5000
+    
+    def getHeading(i):
+        # function to calculate the landscape heading
+        dy = -ypos[i] + f_interp(xpos[i])
+        dx = 0.05
+        angle = atan2(dy,dx)
+        return math.atan2(math.sin(angle),math.cos(angle))
+     
+    
+    speed=1
     
     
+    # simulate individual movement
+    for t in range(TIMESTEPS):
+    
+        for i in range(N):
+            new_angle = getHeading(i)
+            heading[i] =math.atan2(math.sin(new_angle),math.cos(new_angle))
 
-    
-    if t>1000:
-        if t%10==0:
-            plt.clf()
-            plt.plot(xpos, ypos,'k.')
-            xc = np.mean(xpos)
-            plt.xlim([20,40])
-            plt.ylim([45,55])
-            plt.axes().set_aspect('equal')
-            plt.draw()
-            plt.pause(0.01)
-            # plot the positions of all individuals
-        newcpos = pd.DataFrame(np.column_stack((np.full(N,t,dtype='int64'),xpos,ypos,heading,np.arange(0,N))), columns= ['frame','x','y','heading','c_id'])  
+        xpos = xpos + dt*speed*np.cos(heading)
+        ypos = ypos + dt*speed*np.sin(heading)
+        
+    #=====plot if necessary ========================================================
+#        if (t% 5)==0:
+#            if np.min(xpos)<20:
+#                  ##plot the positions of all individuals
+#                plt.clf()
+#                plt.plot(xpos, ypos,'k.')
+#                 
+#                plt.xlim([0,20])
+#                plt.ylim([-6,6])
+#                plt.axes().set_aspect('equal')
+#                plt.draw()
+#                plt.pause(0.01)
+#    #==============================================================================
+        
+        outTime = np.full(N,t,dtype='int64')
+        clipper = np.full(N,clip,dtype='int64')
+        outID = np.arange(0,N)
+        outputInd = (xpos>10) & (xpos<100)
+        newcpos = pd.DataFrame(np.column_stack((outTime[outputInd],xpos[outputInd],ypos[outputInd],heading[outputInd],outID[outputInd],clipper[outputInd])), columns= ['frame','x','y','heading','c_id','clip'])  
         outTracks = outTracks.append(newcpos,ignore_index=True )
-
+    
 
 outTracks.to_csv(trackName, index=False)    
